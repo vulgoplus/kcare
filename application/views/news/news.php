20dcf044a4135eabc4e9773422bdc41c948ddfc8
Emperor <vulgoplus@gmail.com>
2017-01-20 17:13:04 +0700

    Modified News page

--- application/views/news/news.php
+++ application/views/news/news.php
@@ -4,75 +4,49 @@
 	<title>Trang thông tin - Bảo việt K-Care</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/bootstrap.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/slick.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/slick-theme.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/news/animate.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/news/style.css') ?>">
 	<script type="text/javascript" src="<?php echo base_url('public/js/jquery-2.0.0.min.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('public/js/jQuery.scrollSpeed.js') ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('public/js/slick.min.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('public/js/news/wow.min.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('public/js/news/custom.js') ?>"></script>
 </head>
 <body>
 <div class="wrapper">
 	<div class="header">
-		<div class="inside">
-			<div class="banner">
-				<div class="logo">
-					<img src="<?php echo base_url('public/images/logo.png') ?>">
-				</div>
-				<div class="sologan">
-					An tâm gánh nặng chia sẻ
-				</div>
-			</div>
-			<div class="feature-news">
-				<h2>Nổi bật</h2>
-				<div id="feature-carousel">
-					<?php foreach ($featured as $item): ?>
-						<div class="carousel-item">
-							<a href="#">
-								<img src="<?php echo base_url('uploads/news/300x100/'.$item['image']) ?>">
-								<div class="carousel-title">
-									<?php echo $item['title'] ?>
-								</div>
-							</a>
-						</div>
-					<?php endforeach ?>
-				</div>
-			</div>
-		</div><!--End inside-->
-	</div><!--End header-->
-	<div class="new-post">
-		<h1>Có gì mới?</h1>
-	</div>
-	<div class="posts-list">
-		<?php $i = 1; ?>
-		<?php foreach ($new_posts as $item): ?>
-			<div class="post-item item-<?php echo $i ?> wow fadeInDown">
-				<img src="<?php echo base_url('uploads/news/900x300/'.$item['image']) ?>">
-				<div class="post-title">
-					<a href="<?php echo base_url('news/single/'.$item['alias']) ?>"><?php echo $item['title'] ?></a>
-				</div>
-				<div class="post-sumary">
-					<p>
-						<?php echo $item['sumary'] ?>
-					</p>
-					<a href="<?php echo base_url('news/single/'.$item['alias']) ?>" class="read-more">Đọc tiếp</a>
-				</div>
-			</div>
-		<?php $i++; endforeach ?>
-		<br clear="all">
-		<div class="pagination">
-			<a href="#" class="page">1</a>
-			<a href="#" class="page">2</a>
-			<a href="#" class="page">3</a>
+		<div class="logo">
+			<img src="<?php echo base_url('public/images/logo.png') ?>">
 		</div>
 	</div>
-	<br clear="all">
-	<div class="footer">
-		
+	<div class="body">
+		<div class="left-col">
+			<div class="post-item">
+				<div class="img-wrapper">
+					<img src="<?php echo base_url('uploads/news/900x300/avat.jpg') ?>" class="img img-responsive">
+				</div>
+				<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
+				<span class="date">Đăng ngày: 19/01/2016</span>
+				<p style="margin-top: 20px">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat....
+				</p>
+				<a href="#" class="read-more">Đọc tiếp</a>
+			</div>
+
+			<hr style="color: #8cb8ff">
+			<div class="k-pagination">
+				<a href="#">1</a>
+				<a href="#">2</a>
+				<a href="#">3</a>
+				<a href="#">4</a>
+			</div>
+		</div><!--End left-col-->
+		<div class="right-col">
+			
+		</div><!--End right-col-->
 	</div>
-</div><!--End wrapper-->
+</div>
 </body>
 </html>
--- public/js/news/custom.js
+++ public/js/news/custom.js
@@ -5,37 +5,6 @@ $(document).ready(function(){
         parallax();
     });
 
-    $('#feature-carousel').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        adaptiveHeight: true,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                arrows: false
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false
-            }
-        }]
-    });
-
     wow = new WOW().init();
 
     maxWith = 0;
